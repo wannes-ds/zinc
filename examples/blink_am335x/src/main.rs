35cefca67f7ea753f02af4b0c715259596d49f64
Wannes <wannes.de.smet@be-mobile.com>
2017-10-01 00:53:12 +0200

    UART demo, fix crappy linker handling in cargo/rustc by using linker script

--- examples/blink_am335x/src/main.rs
+++ examples/blink_am335x/src/main.rs
@@ -5,7 +5,8 @@
 extern crate zinc;
 
 use zinc::hal::pin::{Gpio, GpioDirection};
-use zinc::hal::am335x::pin;
+use zinc::hal::am335x::{gpio, uart};
+use zinc::drivers::chario::CharIO;
 
 #[zinc_main]
 pub fn main() {
@@ -13,21 +14,25 @@ pub fn main() {
     zinc::hal::mem_init::init_data();
     zinc::hal::am335x::init::enable_vfp();
     zinc::hal::am335x::peripheral_clock::PeripheralClockDomain::gpio(1).enable();
+    zinc::hal::am335x::wakeup_clock::WakeUpClockDomain::uart0().enable();
 
-    let led1 = pin::Pin {
-        module: pin::Module::Module1,
+    let led1 = gpio::GpioPin {
+        module: gpio::Module::Module1,
         pin: 21u32,
         mode: GpioDirection::Out
     };
     led1.setup();
 
+    let uart0 = uart::UART::new(uart::UARTModule::Module0);
+    uart0.start();
+
     let div = 8.6;
     let mut i = 0.1;
 
+    led1.set_high();
+
     loop {
-        led1.set_high();
-        let test = "moop";
-        let float_test = i / 8.6;
-        i = i + 1.0;
+        // :)
+        uart0.puts("I am a little panda 🐼\r\n");
     }
 }
